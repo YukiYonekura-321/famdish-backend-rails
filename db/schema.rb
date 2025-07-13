a807304f112c693a509f448a0392cccd4bee8117
YukiYonekura <yoneyuu321@gmail.com>
2025-07-13 15:02:33 +0900

    👔 Userモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_11_113416) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_13_001624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "menus", force: :cascade do |t|
     t.string "menu"
     t.string "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firebase_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
