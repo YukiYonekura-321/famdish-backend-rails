dc6f09746d5ea64a7e79c8859d03309a23531782
YukiYonekura <yoneyuu321@gmail.com>
2025-07-17 08:03:09 +0900

    ✨ 証明書(公開鍵)初回取得→Redisにキャッシュ

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -2,3 +2,10 @@ FirebaseIdToken.configure do |config|
   config.redis = Redis.new
   config.project_ids = [ "famdish-6f806" ]  # ← Firebase プロジェクトIDを設定
 end
+
+# 証明書を事前取得してキャッシュ
+begin
+  FirebaseIdToken::Certificates.request!
+rescue => e
+  Rails.logger.warn "証明書の取得に失敗しました: #{e.message}"
+end
