22d407cb1371d6c43bed1e35e39be82386b9043f
YukiYonekura <yoneyuu321@gmail.com>
2025-10-20 18:06:17 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :menus, only: [ :index ]
+    resources :menus, only: [ :index, :show, :create, :update, :destroy ]
+    resources :members, only: [ :index, :show, :create, :update, :destroy ]
+    resources :likes, only: [ :index ]
   end
 end
