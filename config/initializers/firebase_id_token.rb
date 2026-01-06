7413b0f7fb0a23e39af24ce5acd254145ac2a290
YukiYonekura <yoneyuu321@gmail.com>
2026-01-07 07:58:38 +0900

    🔥 不要な設定を削除

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,15 +1,7 @@
-require "resolv"
-
-# IPv6 を無効化
-Resolv::IPv6.disable
-
 FirebaseIdToken.configure do |config|
   config.redis = Redis.new(url: ENV["REDIS_URL"])
   config.project_ids = [ "famdish-6f806" ]  # ← Firebase プロジェクトIDを設定
   # タイムアウトを少し長めに
-  config.request_timeout = 5
-  config.http_open_timeout = 3  # 接続タイムアウト
-  config.http_read_timeout = 3  # 読み取りタイムアウト
 end
 
 # 証明書を事前取得してキャッシュ
