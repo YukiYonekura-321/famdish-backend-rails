e374edb6af1c9e6f4ed9a90b29c6d262d6068457
YukiYonekura <yoneyuu321@gmail.com>
2026-01-31 16:00:45 +0900

    🔧 Heroku Redis仕様に変更(Heroku Redisは自己署名であるため、Verify_noneを適用)

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,5 +1,12 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(url: ENV["REDIS_URL"])
+  # config.redis = Redis.new(url: ENV["REDIS_URL"])
+  uri = URI.parse(ENV["REDIS_URL"])
+
+  config.redis = Redis.new(
+    url: ENV["REDIS_URL"],
+    ssl: uri.scheme == "rediss",
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # ←自己署名対応
+  )
   config.project_ids = [ "famdish-6f806" ]  # ← Firebase プロジェクトIDを設定
   # タイムアウトを少し長めに
 end
