963e2f6de0cbe458f27a5ef4d0814624e73ebdcf
YukiYonekura <yoneyuu321@gmail.com>
2026-02-05 07:41:33 +0900

    🏗️ モデル設計を変更

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,4 @@
 class Family < ApplicationRecord
-  belongs_to :user   # 家族の管理者
+  has_many :users   # 家族の管理者
   has_many :members
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :firebase_uid, presence: true, uniqueness: true
-  has_one :family
+  belongs_to :family
+  has_many :members, through: :family
 end
