e66d69cc8791b29da13ac4621d0421101836de22
YukiYonekura <yoneyuu321@gmail.com>
2026-02-06 16:46:12 +0900

    ✨ 新規ルーティング追加/api/members/me

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :menus, only: [ :index, :show, :create, :update, :destroy ]
-    resources :members, only: [ :index, :show, :create, :update, :destroy ]
+    resources :members, only: [ :index, :show, :create, :update, :destroy ] do
+      # GET /api/members/me
+      get :me, on: :collection
+    end
     resources :likes, only: [ :index ]
     resources :suggestions, only: [:create, :update] do
       member do
