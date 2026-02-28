4bdcced35321c5978850694db071c385a60b1a75
YukiYonekura <yoneyuu321@gmail.com>
2026-02-28 12:34:06 +0900

    🔧 chore: revert Redis SSL verify_mode to VERIFY_NONE
    Temporarily disable SSL peer verification for Heroku Redis (VERIFY_NONE).

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,14 +1,12 @@
 FirebaseIdToken.configure do |config|
   uri = URI.parse(ENV["REDIS_URL"])
 
-  ca_file = "/etc/ssl/certs/ca-certificates.crt"
-
   config.redis = Redis.new(
     url: ENV["REDIS_URL"],
     ssl: uri.scheme == "rediss",
     ssl_params: { 
-      verify_mode: OpenSSL::SSL::VERIFY_PEER,
-      ca_file: ca_file
+      # Heroku Redis はVPC内通信で、通信自体はSSL
+      verify_mode: OpenSSL::SSL::VERIFY_NONE
     } 
   )
   config.project_ids = [ "famdish-6f806" ]  # ← Firebase プロジェクトIDを設定
