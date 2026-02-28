7f756d882ae44ad07ace5fc92a41abdd73b99dfc
YukiYonekura <yoneyuu321@gmail.com>
2026-02-28 11:46:12 +0900

    🔧 chore: use system CA bundle for Redis SSL (VERIFY_PEER)

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,19 +1,15 @@
 FirebaseIdToken.configure do |config|
-  # config.redis = Redis.new(url: ENV["REDIS_URL"])
   uri = URI.parse(ENV["REDIS_URL"])
+
+  ca_file = "/etc/ssl/certs/ca-certificates.crt"
 
   config.redis = Redis.new(
     url: ENV["REDIS_URL"],
     ssl: uri.scheme == "rediss",
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # ←自己署名対応
+    ssl_params: { 
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,
+      ca_file: ca_file
+    } 
   )
   config.project_ids = [ "famdish-6f806" ]  # ← Firebase プロジェクトIDを設定
-  # タイムアウトを少し長めに
 end
-
-# # 証明書を事前取得してキャッシュ
-# begin
-#   FirebaseIdToken::Certificates.request
-# rescue => e
-#   Rails.logger.warn "証明書の取得に失敗しました: #{e.message}"
-# end
