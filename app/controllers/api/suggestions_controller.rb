d2014574585549038f81e6b7ed92e664bc5b019f
YukiYonekura <yoneyuu321@gmail.com>
2026-03-02 17:44:18 +0900

    ⏪ revert(suggestions): fetch_feedback の安全化変更を元に戻す

--- app/controllers/api/suggestions_controller.rb
+++ app/controllers/api/suggestions_controller.rb
@@ -88,9 +88,7 @@ module Api
     end
 
     def fetch_feedback(sg_id)
-      return {} if sg_id.blank?
-
-      Suggestion.find_by(id: sg_id)&.feedback || {}
+      sg_id.present? ? Suggestion.find(sg_id).feedback : {}
     end
   end
 end
