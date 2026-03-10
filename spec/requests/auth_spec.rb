9363cec5ad22d3b0034b9a5bb5ca54445ae1bd73
YukiYonekura <yoneyuu321@gmail.com>
2026-03-10 12:19:51 +0900

    ✅ test(auth): Firebase認証の証明書エラーリカバリテストを追加
    NoCertificatesErrorおよびCertificateExpiredError発生時の
    自動リトライ処理と、予期しないエラーハンドリングをテスト。
    application_controllerの認証ロジックカバレッジを向上

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -35,5 +35,58 @@ RSpec.describe "認証", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    context "証明書エラーからのリカバリ" do
+      let!(:user) { create(:user, firebase_uid: "recover-uid") }
+      let!(:family) { create(:family) }
+      let!(:member) { create(:member, family: family, user: user) }
+
+      before { user.update!(family: family) }
+
+      it "NoCertificatesError 時に証明書を再取得して認証成功する" do
+        call_count = 0
+        allow(FirebaseIdToken::Signature).to receive(:verify) do
+          call_count += 1
+          if call_count == 1
+            raise FirebaseIdToken::Exceptions::NoCertificatesError
+          else
+            { "user_id" => "recover-uid" }
+          end
+        end
+        allow(FirebaseIdToken::Certificates).to receive(:request!)
+
+        get "/api/stocks", headers: { "Authorization" => "Bearer some-token" }
+        expect(response).to have_http_status(:ok)
+        expect(FirebaseIdToken::Certificates).to have_received(:request!)
+      end
+
+      it "CertificateExpiredError 時に証明書を再取得して認証成功する" do
+        call_count = 0
+        allow(FirebaseIdToken::Signature).to receive(:verify) do
+          call_count += 1
+          if call_count == 1
+            raise FirebaseIdToken::Exceptions::CertificateExpiredError
+          else
+            { "user_id" => "recover-uid" }
+          end
+        end
+        allow(FirebaseIdToken::Certificates).to receive(:request!)
+
+        get "/api/stocks", headers: { "Authorization" => "Bearer some-token" }
+        expect(response).to have_http_status(:ok)
+        expect(FirebaseIdToken::Certificates).to have_received(:request!)
+      end
+    end
+
+    context "予期しない認証エラー" do
+      it "一般的な例外で nil を返し 401 になる" do
+        allow(FirebaseIdToken::Signature).to receive(:verify)
+          .and_raise(StandardError, "unexpected error")
+        allow(FirebaseIdToken::Certificates).to receive(:request!)
+
+        get "/api/stocks", headers: { "Authorization" => "Bearer some-token" }
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
   end
 end
