3a06b80b385e06d39306dd3f91cae24e5d483566
YukiYonekura <yoneyuu321@gmail.com>
2026-03-14 23:03:06 +0900

    🐛 fix(coverage): SimpleCov の初期化順序を修正
    environment ロード前に SimpleCov を初期化することで、
    アプリケーションコード全体がカバレッジ計測に含まれるように修正。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 
-# SimpleCov (テストカバレッジ計測)
+# SimpleCov (テストカバレッジ計測) - 環境ロード前に初期化
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/spec/'
@@ -17,6 +13,11 @@ SimpleCov.start 'rails' do
     FileUtils.mkdir_p(coverage_dir) unless Dir.exist?(coverage_dir)
   end
 end
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
 
 # Support ファイルを自動読み込み
 Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
