7b9d6b84c660c0b1d3fff9876a49a0bb2aa15ee9
YukiYonekura <yoneyuu321@gmail.com>
2026-05-31 18:49:18 +0900

    feat: Add image_url column to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_31_014326) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_31_044236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -95,6 +95,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_31_014326) do
     t.text "reason"
     t.bigint "family_id"
     t.bigint "suggestion_id"
+    t.string "image_url"
     t.index ["suggestion_id"], name: "index_recipes_on_suggestion_id"
   end
 
